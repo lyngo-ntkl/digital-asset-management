82af15c8d82fd169a4ddb6611f7684a8aafd804e
lyngo-ntkl <lyngo.ntkl@gmail.com>
2024-08-30 17:39:49 +0700

    config: swagger authorization header

--- DigitalAssetManagement.API/DependencyInjection.cs
+++ DigitalAssetManagement.API/DependencyInjection.cs
@@ -1,4 +1,5 @@
 ﻿using DigitalAssetManagement.API.Common;
+using Microsoft.OpenApi.Models;
 
 namespace DigitalAssetManagement.API
 {
@@ -7,7 +8,33 @@ namespace DigitalAssetManagement.API
         public static IServiceCollection AddAPI(this IServiceCollection services)
         {
             // swagger
-            services.AddSwaggerGen();
+            services.AddSwaggerGen(options =>
+            {
+                options.SwaggerDoc("v1", new OpenApiInfo { Title = "Digital Asset Management API", Version = "v1" });
+                options.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
+                {
+                    In = ParameterLocation.Header,
+                    Description = "Enter JWT",
+                    Name = "Authorization",
+                    Type = SecuritySchemeType.Http,
+                    BearerFormat = "JWT",
+                    Scheme = "Bearer"
+                });
+                options.AddSecurityRequirement(new OpenApiSecurityRequirement
+                {
+                    {
+                        new OpenApiSecurityScheme
+                        {
+                            Reference = new OpenApiReference
+                            {
+                                Type = ReferenceType.SecurityScheme,
+                                Id = "Bearer"
+                            }
+                        },
+                        new string[] {}
+                    }
+                });
+            });
 
             // controllers
             services.AddControllers();
